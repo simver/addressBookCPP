146528315d1488515c314683ed1b336d8f6f7ec5
Ryan <simvers@outlook.com>
2020-07-15 02:53:37 +0800

    ADD:初始化

---
+++ AddressBookCPP/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct address {
+	string name;
+	int phone;
+};
+
+int main() {
+	string test = "test";
+	cout << "test" << endl;
+}
